7f7dc6b75af618152b079efc68a8d1d220273242
nmahesh1412 <37714229+nmahesh1412@users.noreply.github.com>
2021-04-30 12:40:54 -0700

    Update map.js

--- map.js
+++ map.js
@@ -67,6 +67,7 @@ function createMarker(map, location, infowindow) {
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent('<div>'+
     '<p><strong>' + ((location.url === undefined) ? location.title : ('<a href="' + location.url +'">' + location.title + '</a>')) + '</strong></p>' +
+    ((location.covidbed === undefined) ? "" : ('<p><strong>COVID Beds: </strong>' + location.covidbed + '</p>')) +
     '</div>');
     infowindow.open(map, marker);
   });
