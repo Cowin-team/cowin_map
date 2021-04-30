3409153c3f9879ed4a12522a3b34fa953e27f6b2
nmahesh1412 <37714229+nmahesh1412@users.noreply.github.com>
2021-04-30 10:36:30 -0700

    Update map.js

--- map.js
+++ map.js
@@ -23,6 +23,7 @@ function initialiseMap() {
         // location.department = this[4];
         // location.funder = this[0];
         location.url = this[4];
+	location.covidbed = this[5];
           locations.push(location);
     	});
       console.log(locations)
