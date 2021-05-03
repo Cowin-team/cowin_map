4f2ef1188ecf8e859554ec93774db9e5c10b53e3
Sri Vishnu Totakura <srivishnu@totakura.in>
2021-05-03 21:34:30 +0200

    Restructure filters

---
+++ assets/js/src/bulmaInteractions.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  let dropdowns = document.querySelectorAll('.dropdown');
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', function(event) {
+      event.stopPropagation();
+      dropdown.classList.toggle('is-active');
+    });
+  });
+});
---
+++ assets/js/src/filters/mapFilters.js
@@ -0,0 +1,38 @@
+class MapFilters {
+  constructor(options) {
+    this.state = []
+    options.filterNames.forEach((filterName) => {
+      this.state[filterName] = false
+    })
+    this.setEventListeners(options.filtersElemSelector);
+    this.onChangeCallback = options.onChangeCallback;
+  }
+
+  setEventListeners(filtersElemSelector) {
+    let that = this;
+    document.querySelectorAll(filtersElemSelector).forEach(function(filter_chkbx) {
+      filter_chkbx.addEventListener("change", (event) => { that.filterChanged(event.target) });
+    });
+  }
+
+  anyApplied() {
+    let isAnyApplied = false;
+
+    for (const filterName in this.state) {
+      if (this.state[filterName] == true) {
+        isAnyApplied = true;
+        break;
+      }
+    }
+    return isAnyApplied;
+  }
+
+  filterChanged(checkedCheckboxElem) {
+    let filterName = checkedCheckboxElem.dataset.filterName;
+    this.state[filterName] = checkedCheckboxElem.checked;
+
+    this.onChangeCallback(this.state);
+  }
+}
+
+export default MapFilters;
