631d188550a90b993b124dbc6841b4de3c727421
Bhairevi G Aiyer <bhairevigaiyer94@gmail.com>
2021-06-13 20:19:36 +0530

    removed bootstrap, changed to bulma

--- assets/js/lib/bulmaInteractions.js
+++ assets/js/lib/bulmaInteractions.js
@@ -8,3 +8,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
   });
 });
+
+
+const targetDivM = document.getElementById("toggledDetailsMobile");
+const targetDivD = document.getElementById("toggledDetailsDesktop");
+const btn = document.getElementById("toggleButton");
+btn.onclick = function () {
+if (targetDivM.style.display !== "none") {
+targetDivM.style.display = "none";
+} else {
+targetDivM.style.display = "block";
+}
+if (targetDivD.style.display !== "none") {
+  targetDivD.style.display = "none";
+  } else {
+  targetDivD.style.display = "block";
+  }
+};
